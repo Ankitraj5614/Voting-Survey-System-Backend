ea5836bc67d38c762506ac302df23d2fbf8cd94f
Ankit Raj <119001934+Ankitraj5614@users.noreply.github.com>
2026-03-09 05:57:57 +0530

    add controller

---
+++ vortingsurvey/src/main/java/com/jspider/vortingsurvey/VortingsurveyApplication.java
@@ -0,0 +1,13 @@
+package com.jspider.vortingsurvey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VortingsurveyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VortingsurveyApplication.class, args);
+	}
+
+}
---
+++ vortingsurvey/src/test/java/com/jspider/vortingsurvey/VortingsurveyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jspider.vortingsurvey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VortingsurveyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
