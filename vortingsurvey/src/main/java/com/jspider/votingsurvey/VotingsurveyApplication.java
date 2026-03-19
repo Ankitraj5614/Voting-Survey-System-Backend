196058ab5bb63ac06aecf891b632096618902591
Ankit Raj <119001934+Ankitraj5614@users.noreply.github.com>
2026-03-19 23:04:22 +0530

    adding interface Admin Repository

--- vortingsurvey/src/main/java/com/jspider/votingsurvey/VotingsurveyApplication.java
+++ vortingsurvey/src/main/java/com/jspider/votingsurvey/VotingsurveyApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class VortingsurveyApplication {
+public class VotingsurveyApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(VortingsurveyApplication.class, args);
+		SpringApplication.run(VotingsurveyApplication.class, args);
 	}
 
 }
